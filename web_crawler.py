4bc3ea68e0b7165445847285b0aa7cada5b913de
Vaibhav Agrawal <vb8817@gmail.com>
2017-03-26 10:22:29 +0530

    Update web_crawler.py

--- web_crawler.py
+++ web_crawler.py
@@ -20,5 +20,18 @@ def trade_spider(max_pages):
 			
 		fw.close()
 		page += 1
+		
+def get_single_item_data(item_url):
+    source_code = requests.get(item_url)
+    plain_text = source_code.text
+    soup = BeautifulSoup(plain_text,"lxml")
+    # if you want to gather photos from that user
+    for item_name in soup.findAll('img', {'class': 'img-responsive'}): # all photos of the user
+        photo='https://thenewboston.com'+item_name.get('src')
+        print(photo)
+    # if you want to gather links for a web crawler
+    for link in soup.findAll('a'):
+        href = link.get('href')
+        print(href)
 
 trade_spider(3)
